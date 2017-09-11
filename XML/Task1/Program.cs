199312012c713682229287f44bbbb634f40864fb
AlisherB <alisher_b_95@mail.ru>
2017-09-11 22:03:54 +0600

    HW_XML
    Домашнее задание(XML)

---
+++ XML/Task1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+using static System.Console;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Item> items = new List<Item>();
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("https://habrahabr.ru/rss/interesting/");
+            XmlElement root = xmlDoc.DocumentElement;
+            XmlElement channel = root["channel"];
+            if (channel.HasChildNodes)
+            {
+                foreach (XmlNode node in channel.ChildNodes)
+                {
+                    if (node.Name == "item")
+                    {
+                        Item item = new Item();
+                        item.Title = node["title"].InnerText;
+                        item.Link = node["link"].InnerText;
+                        item.Description = node["description"].InnerText;
+                        item.PubDate = node["pubDate"].InnerText;
+                        items.Add(item);
+                    }
+                }
+            }
+            foreach (var i in items)
+            {
+                WriteLine(i.Title);
+                WriteLine(i.Link);
+                WriteLine(i.Description);
+                WriteLine(i.PubDate);
+            }
+            ReadLine();
+        }
+    }
+}
---
+++ XML/Task2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using static System.Console;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("Student.xml");
+            Student student = new Student();
+            XmlElement element = xmlDoc.DocumentElement;
+
+            XmlNode classStudent = xmlDoc.CreateElement("Student");
+            xmlDoc.DocumentElement.AppendChild(classStudent);
+
+            Write("Введите имя: ");
+            student.Name = ReadLine();
+            XmlNode name = xmlDoc.CreateElement("name");
+            name.InnerText = student.Name;
+            classStudent.AppendChild(name);
+
+            Write("Введите фамилию: ");
+            student.Surname = ReadLine();
+            XmlNode surname = xmlDoc.CreateElement("surname");
+            surname.InnerText = student.Surname;
+            classStudent.AppendChild(surname);
+
+            Write("Введите возраст: ");
+            student.Age = ReadLine();
+            XmlNode age = xmlDoc.CreateElement("age");
+            age.InnerText = student.Age;
+            classStudent.AppendChild(age);
+
+            Write("Введите специальность: ");
+            student.Speciality = ReadLine();
+            XmlNode speciality = xmlDoc.CreateElement("speciality");
+            speciality.InnerText = student.Speciality;
+            classStudent.AppendChild(speciality);
+            
+            xmlDoc.Save("Student.xml");
+            ReadLine();
+        }
+    }
+}
